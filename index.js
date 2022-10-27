137197eea806e093879e19bebf296fb852c48f7b
aminul <accounreate@gmail.com>
2022-10-27 23:39:00 +0600

    everything completed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+app.use(cors())
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+
+const news = require('./data/news.json');
 
 const categories = require('./data/categories.json');
 
@@ -11,8 +13,30 @@ app.get('/',(req, res)=>{
    res.send('New API Running');
 })
 
-app.get('/categories',(req, res)=>{
+app.get('/news', (req, res)=>{
+   res.send(news)
+})
+
+app.get('/news-categories',(req, res)=>{
    res.send(categories);
+})
+
+app.get('/category/:id', (req, res)=>{
+   const id = req.params.id;
+   if(id === '08'){
+      res.send(news)
+   }
+   else{
+      const category_news = news.filter(n=>n.category_id === id);
+      res.send(category_news)
+      // console.log(category_news);
+   }
+})
+
+app.get('/news/:id', (req, res)=>{
+   const id = req.params.id;
+   const selectedNews = news.find(n=> n._id===id);
+   res.send(selectedNews);
 })
 
 app.listen(port, ()=>{
